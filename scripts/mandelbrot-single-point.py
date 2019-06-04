578af4853a6bd49a514ea333c36ff3999060b143
Daniel Kirchner <daniel97k@gmail.com>
2019-06-04 22:10:48 +0200

    weiter

--- scripts/mandelbrot-single-point.py
+++ scripts/mandelbrot-single-point.py
@@ -1,10 +1,9 @@
 def mandelbrot(z):
     c = z
-    for n in range(100):
-        if abs(z) > 2:
-            return n
+    for n in range(1,11):
+        print(n,z,abs(z))
         z = z*z + c
 
 if __name__ == "__main__":
-    c = complex(1,1)
+    c = complex(1,0)
     mandelbrot(c)
