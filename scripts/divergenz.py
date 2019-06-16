e0a32658c1df544b4b32d5b96ba4980cef6de07b
Daniel Kirchner <daniel97k@gmail.com>
2019-06-16 17:15:38 +0200

    fertig hausdorff, anfang mandelbrot

--- scripts/divergenz.py
+++ scripts/divergenz.py
@@ -1,8 +1,12 @@
 import matplotlib.pyplot as plt
+import matplotlib
 iterations = 200
 
 def getPoints(c):
+    print(c)
+    c = complex(-0.55,0.46) #ÄNDERN
     z = complex(0,0)
+
     pointsx = []
     pointsy = []
     for i in range(0,iterations):
@@ -19,14 +23,16 @@ def onclick(event):
         plt.xlabel("real")
         plt.ylabel("imaginär")
 
-        plt.scatter(pointsx,pointsy,s=1,c="black") #einzelne iterationspunkte
-        plt.scatter(event.xdata,event.ydata,c="red",s=20) #startpunkt
-        plt.scatter(pointsx[len(pointsx)-1],pointsy[len(pointsy)-1],c="green",s=20) #konvergenz
+        plt.scatter(pointsx,pointsy,s=3,c="black") #einzelne iterationspunkte
+        #plt.scatter(event.xdata,event.ydata,c="red",s=20) #startpunkt
+        plt.scatter(-0.55,0.46,c="red",s=20)
+        #plt.scatter(pointsx[len(pointsx)-1],pointsy[len(pointsy)-1],c="green",s=20) #konvergenz
 
-        plt.xlim(-2,2)
-        plt.ylim(-2,2)
+        plt.xlim(-0.8,0.0)
+        plt.ylim(-0.1,0.7)
         plt.draw()
 
 fig,ax=plt.subplots()
 fig.canvas.mpl_connect('button_press_event',onclick)
+matplotlib.rcParams.update({'font.size': 22})
 plt.show()
